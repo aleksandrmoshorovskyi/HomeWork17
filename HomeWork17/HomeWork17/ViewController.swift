2b17c517cedd1aa28baf9884d30d0b2ecf9425a4
Oleksandr Moshorovskyi <46052383+aleksandrmoshorovskyi@users.noreply.github.com>
2024-04-20 17:32:52 +0300

    17.8.1

--- HomeWork17/HomeWork17/ViewController.swift
+++ HomeWork17/HomeWork17/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tableAdapter: TableViewAdapterInput!
+    
+    var models: [TitleCellDisplayModel] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        models = DataLoader().getTitleData()
+        
+        let titlrSection = TitleSectionAdapter(output: self)
+        
+        let adapter = TableViewAdapter()
+        adapter.sections = [
+            titlrSection
+        ]
+        
+        tableAdapter = adapter
+        tableAdapter.setup(with: tableView)
     }
-
-
 }
 
+// MARK: TitleSectionAdapterOutput
+extension ViewController: TitleSectionAdapterOutput {
+    
+    func titleItemsCount() -> Int {
+        
+        return models.count
+    }
+    
+    func titleItem(at indexPath: IndexPath) -> TitleCellDisplayModel {
+        
+        return models[indexPath.row]
+    }
+    
+    func goAction(at indexPath: IndexPath) {
+        
+        let item = models[indexPath.row]
+        
+        print(item.titleText)
+    }
+}
